c4b09f3f775d77c3258088cf2b5158bd44b14a3c
ganeshbalan86-collab <ganeshbalan86-collab@users.noreply.github.com>
2026-04-06 19:11:10 +0530

    Bump service worker cache to force refresh

--- www/sw.js
+++ www/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bubble-v1';
+const CACHE = 'bubble-v2';
 const ASSETS = ['/', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
